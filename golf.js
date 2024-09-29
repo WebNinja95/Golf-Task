93da0e61b6f2e68360b5d60e69d3d3f2dd959be0
WebNinja95 <avivsalem95@gmail.com>
2024-09-29 15:54:48 +0300

    new updata almost donegit add .

--- golf.js
+++ golf.js
@@ -5,8 +5,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
-const cards_board = ['Face Down','Face Down','Face Down','Face Down'];
+const PlayersBoard = [['Face Down','Face Down','Face Down','Face Down'],['Face Down','Face Down','Face Down','Face Down']];
 const Discard_Pile = []
 const cards = {
     hearts: {
@@ -79,17 +78,16 @@ function shuffleDeck(cards) {
     const deck = [];
     for (let suit in cards) {
         for (let card in cards[suit]) {
-            deck.push({ name: card, value: cards[suit][card] });
+            deck.push(card); 
         }
     }
 
-    
     for (let i = 0; i < deck.length; i++) {
         const j = Math.floor(Math.random() * deck.length);
         [deck[i], deck[j]] = [deck[j], deck[i]];
     }
 
-    return deck;  
+    return deck;
 }
 
 function Player(name){
@@ -119,13 +117,91 @@ function CreatePlayersAndDeal() {
             console.log(`Brilliant! Players created: ${players[0].name} and ${players[1].name}`);
             let deck = shuffleDeck(cards);
             deck = dealCardsToPlayers(deck, players);
-            console.log(`${players[0].name} has the following cards:`, players[0].cards);
-            console.log(`${players[1].name} has the following cards:`, players[1].cards);
-
-            rl.close(); 
+            console.log(`${players[0].name} created `);
+            console.log(`${players[1].name} created`);
+            Discard_Pile.push(deck.pop());
+            StartGame(deck, Discard_Pile, players, PlayersBoard, 0);
         });
     });
 }
 
+function CheckFaceDown(PlayersBoard){
+    let ct1 = 0;
+    let ct2 = 0;
+    for(let i = 0; i < 4; i++){
+        if(PlayersBoard[0][i] !== 'Face Down'){
+            ct1++;
+        }
+        if(PlayersBoard[1][i] !== 'Face Down'){
+            ct2++;
+        }
+    }
+    return (ct1 === 4 || ct2 === 4);
+}
+
+function FinalScore(players){
+    let Player1Score = 0;
+    let Player2Score = 0;
+    
+    console.log(`${players[0].name} final score: ${Player1Score}`);
+    console.log(`${players[1].name} final score: ${Player2Score}`);
+    return true;
+}
+
+function StartGame(deck, Discard_Pile, players, PlayersBoard, PlayTurn){
+    const Check = CheckFaceDown(PlayersBoard);
+    if(Check){
+        return FinalScore(players);
+    }
+    if(PlayTurn > 1){
+        PlayTurn = 0;
+    }
+    console.log(`${players[0].name} Hands: ${PlayersBoard[0]}`);
+    console.log(`${players[1].name} Hands: ${PlayersBoard[1]}`);
+    console.log(`Discard Pile: ${Discard_Pile[Discard_Pile.length-1]}`);
+    GamePlay(deck, Discard_Pile, players, PlayersBoard, PlayTurn);
+}
+
+function GamePlay(deck, Discard_Pile, players, PlayersBoard, PlayerTurn){
+    console.log(`It's ${players[PlayerTurn].name}'s turn!`);
+    rl.question("Draw from deck (D) or take from discard pile (P)? ", (action) => {
+        if (action.toLowerCase() === 'd') {
+            const drawnCard = deck.pop();
+            console.log(`You drew: ${drawnCard}`);
+
+            rl.question("Do you want to (R) Replace a card or (D) Discard the drawn card? ", (nextAction) => {
+                if (nextAction.toLowerCase() === 'r') {
+                    rl.question("Which card do you want to replace? (1-4): ", (cardPosition) => {
+                        const replacedCard = players[PlayerTurn].cards[cardPosition - 1];
+                        players[PlayerTurn].cards[cardPosition - 1] = drawnCard;
+                        Discard_Pile.push(replacedCard);
+                        PlayersBoard[PlayerTurn][cardPosition - 1] = drawnCard;
+                        console.log(`${players[PlayerTurn].name} replaced a card!`);
+                        StartGame(deck, Discard_Pile, players, PlayersBoard, PlayerTurn + 1);
+                    });
+                } else {
+                    Discard_Pile.push(drawnCard);
+                    console.log(`${players[PlayerTurn].name} discarded the drawn card.`);
+                    StartGame(deck, Discard_Pile, players, PlayersBoard, PlayerTurn + 1);
+                }
+            });
+        } else if (action.toLowerCase() === 'p') {
+            const topDiscardCard = Discard_Pile.pop();
+            console.log(`You took: ${topDiscardCard}`);
+
+            rl.question("Which card do you want to replace? (1-4): ", (cardPosition) => {
+                const replacedCard = players[PlayerTurn].cards[cardPosition - 1];
+                players[PlayerTurn].cards[cardPosition - 1] = topDiscardCard;
+                Discard_Pile.push(replacedCard);
+                console.log(`${players[PlayerTurn].name} replaced a card with a discard pile card!`);
+                PlayersBoard[PlayerTurn][cardPosition - 1] = topDiscardCard;
+                StartGame(deck, Discard_Pile, players, PlayersBoard, PlayerTurn + 1);
+            });
+        } else {
+            console.log("Invalid input. Try again.");
+            StartGame(deck, Discard_Pile, players, PlayersBoard, PlayerTurn);
+        }
+    });
+}
 
 CreatePlayersAndDeal();
